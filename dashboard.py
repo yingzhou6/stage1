3805714870a533230c521b9028435f555e6da490
yingzhou6 <110857859+yingzhou6@users.noreply.github.com>
2022-08-20 15:55:21 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -153,6 +153,9 @@ def update_graph(stock_slctd):
     return fighist
 
 
+app=dash.Dash(__name__)   
+server=app.server
+
 print(df1['Symbols'])
 if __name__ == '__main__':
     app.run_server(debug=True, port=8000)
